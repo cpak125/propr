8f0db2338356777960b894c70de592a9101e5d2a
cpak125 <chris.pak125@gmail.com>
2022-05-27 14:49:49 -0400

    connect to heroku mysql

--- db/connect_db.php
+++ db/connect_db.php
@@ -1,18 +1,26 @@
 <?php
-/* For Codd connection */
-// $servername = "localhost";
-// $username = "cpak8";
-// $password = "cpak8";
-// $dbname = "cpak8";
-
 /* For local connection */
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "project_4";
+// $servername = "localhost";
+// $username = "root";
+// $password = "root";
+// $dbname = "project_4";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+/* Create connection */
+// $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
 
 // Check connection
 if (!$conn) {
